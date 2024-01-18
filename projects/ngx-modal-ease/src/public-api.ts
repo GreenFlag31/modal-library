6a2545244dd52c9199abde7c8ce490a03d3f9d1c
MC <unam31@gmail.com>
2024-01-18 11:31:57 +0100

    Auto-generated commit [ci skip]

--- projects/ngx-modal-ease/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ngx-modal-ease
- */
-
-export * from './lib/modal/modal.service';
--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'ngx-modal-ease' },
-  { path: 'ngx-modal-ease', component: AppComponent },
-];
